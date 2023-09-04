8628d5cbc471639c0f51d62c5c1887fc832e38f7
Friday64 <mcfrantz12@gmail.com>
2023-09-04 19:03:40 -0400

    Change API validation code

--- Cat Image Downloader.py
+++ Cat Image Downloader.py
@@ -5,7 +5,7 @@ from tkinter import ttk, filedialog, messagebox
 from queue import Queue, Empty
 from flickrapi import FlickrAPI, FlickrError
 import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 import threading
 
 # Specify your custom path here
@@ -21,20 +21,22 @@ if not FLICKR_API_KEY or not FLICKR_API_SECRET:
 root = tk.Tk()
 root.title("Flickr Image Downloader")
 
+
 def validate_api_keys():
     try:
-        flickr = FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET, format='parsed-json')
-        flickr.photos.getInfo(photo_id='1')  # This is a dummy API call to validate the keys
-    except FlickrError as e:
-        if '100' in str(e):
-            messagebox.showerror("Invalid API Key", "The API key format is invalid.")
-            root.quit()
+        flickr = FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET,
+                           format='parsed-json')
+        # Use an actual API endpoint for validation
+        response = flickr.photos.getInfo(photo_id='1')
+        if response['stat'] == 'ok':
+            print("API keys are valid.")
         else:
-            messagebox.showerror("API Error", str(e))
+            print("API keys are invalid.")
             root.quit()
     except Exception as e:
-        messagebox.showerror("Unexpected Error", str(e))
+        print("Error validating API keys:", e)
         root.quit()
+
 
 validate_api_keys()
 
@@ -50,10 +52,12 @@ lock = threading.Lock()
 
 executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
 
+
 def set_folder():
     global folder_selected
     folder_selected = filedialog.askdirectory()
     folder_label.config(text=folder_selected)
+
 
 def download_images_from_flickr():
     global serial_number
@@ -61,20 +65,55 @@ def download_images_from_flickr():
     num_of_images = int(images_entry.get())
 
     if not folder_selected:
-        messagebox.showwarning("Folder Not Selected", "Please select a folder to save the images.")
+        messagebox.showwarning("Folder Not Selected",
+                               "Please select a folder to save the images.")
         return
 
     if not search_term:
-        messagebox.showwarning("Search Term Not Provided", "Please provide a search term.")
+        messagebox.showwarning("Search Term Not Provided",
+                               "Please provide a search term.")
         return
 
     if not num_of_images:
-        messagebox.showwarning("Number of Images Not Provided", "Please provide the number of images to download.")
+        messagebox.showwarning("Number of Images Not Provided",
+                               "Please provide the number of images to download.")
         return
+
+    # Reset progress bar
+    progress_bar['value'] = 0
+    countdown_label.config(text="Images Remaining: 0")
+
+    flickr = FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET, format='parsed-json')
+
+    # Modify the parameters to search for Creative Commons licensed photos
+    photos = flickr.photos.search(
+        text=search_term, per_page=num_of_images, page=1, sort='relevance',
+        license='1,2,3,4,5,6',  # This includes various Creative Commons licenses
+        content_type=1  # Only photos
+    )
+
+    if photos and photos['photos']['photo']:
+        photos_list = photos['photos']['photo']
+
+        for photo in photos_list:
+            url = f"https://live.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}_c.jpg"
+            download_queue.put((url, folder_selected))
+
+        countdown_label.config(
+            text=f"Images Remaining: {download_queue.qsize()}")
+
+        # Reset progress bar maximum value
+        progress_bar['maximum'] = download_queue.qsize()
+
+        for i in range(MAX_WORKERS):
+            executor.submit(download_image)
+
+    else:
+        messagebox.showinfo("No Images Found",
+                            "No images found for the provided search term.")
 
     # Reset progress bar and serial number
     progress_bar['value'] = 0
-    serial_number = 0
     countdown_label.config(text="Images Remaining: 0")
 
     flickr = FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET, format='parsed-json')
@@ -99,7 +138,9 @@ def download_images_from_flickr():
             executor.submit(download_image)
 
     else:
-        messagebox.showinfo("No Images Found", "No images found for the provided search term.")
+        messagebox.showinfo("No Images Found",
+                            "No images found for the provided search term.")
+
 
 def download_image():
     while not download_queue.empty():
@@ -120,6 +161,7 @@ def download_image():
                     gui_queue.put_nowait(image_num)
             download_queue.task_done()
 
+
 def check_gui_queue():
     while True:
         try:
@@ -133,6 +175,7 @@ def check_gui_queue():
             if remaining == 0:
                 countdown_label.config(text="All images downloaded!")
     root.after(100, check_gui_queue)
+
 
 # UI Setup
 search_label = ttk.Label(root, text="Search Term:")
@@ -153,10 +196,12 @@ folder_btn.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
 folder_label = ttk.Label(root, text="")
 folder_label.grid(column=1, row=2, sticky=tk.W, padx=5, pady=5)
 
-download_btn = ttk.Button(root, text="Download", command=download_images_from_flickr)
+download_btn = ttk.Button(root, text="Download",
+                          command=download_images_from_flickr)
 download_btn.grid(column=1, row=3, sticky=tk.W, padx=5, pady=20)
 
-progress_bar = ttk.Progressbar(root, orient='horizontal', length=300, mode='determinate')
+progress_bar = ttk.Progressbar(
+    root, orient='horizontal', length=300, mode='determinate')
 progress_bar.grid(column=1, row=4, sticky=tk.W, padx=5, pady=5)
 
 countdown_label = ttk.Label(root, text="")
